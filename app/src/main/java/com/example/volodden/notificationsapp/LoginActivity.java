b139db02a96343d3329a4b61f9285f46c0e86674
volodin <volodin@phystech.edu>
2017-12-21 06:45:06 +0300

    Merge...

--- app/src/main/java/com/example/volodden/notificationsapp/LoginActivity.java
+++ app/src/main/java/com/example/volodden/notificationsapp/LoginActivity.java
@@ -2,18 +2,9 @@ package com.example.volodden.notificationsapp;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Build;
@@ -31,7 +22,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
@@ -42,6 +32,7 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world", "admin@test.ru:testtest"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
